6dd7516b93225e98a9ca21355cc9c4d18817305d
Komakivan <komakechivan555@gmail.com>
2024-01-18 13:59:04 +0530

    added improvements in the error handling middlewar- ZodError

--- server/src/errorHandler.ts
+++ server/src/errorHandler.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express"
 import HttpException, { ErrorCode } from "./exceptions/root"
 import { InternalException } from "./exceptions/internal-errors"
+import { ZodError } from "zod"
+import { UnprocessableEntity } from "./exceptions/validation"
+import { BadRequestException } from "./exceptions/bad-requests"
 
 // custom error handler to wrap all controllers so that "try{} catch(){}" can be skipped in the controllers
 // its asynchronou: takes in a controller as an argument and returns a controller
@@ -13,7 +16,11 @@ import { InternalException } from "./exceptions/internal-errors"
             if(error instanceof HttpException) {
                 exception = error
             } else {
-                exception = new InternalException("something went wrong!",ErrorCode.INTERNAL_EXCEPTION, error)
+                if(error instanceof ZodError) {
+                    exception = new UnprocessableEntity(error, "Unprocessable entity", ErrorCode.UNPROCESSABLE_ENTITY)
+                } else {
+                    exception = new InternalException("something went wrong!",ErrorCode.INTERNAL_EXCEPTION, error)
+                }
             }
             next(exception)
         }
